370652dcda80acbe0c14dccb753165a8426069b2
Shaun Rose <thefrznrose@gmail.com>
2024-11-17 21:40:35 -0800

    Still trying to fix cors

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2/promise');
 const multer = require('multer');
-const cors = require('cors');
+// wconst cors = require('cors');
 require('dotenv').config();
 
 const app = express();

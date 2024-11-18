d6e79bfc26e23dbfd5b614b2a9389c93e293cf14
Shaun Rose <thefrznrose@gmail.com>
2024-11-18 11:18:55 -0800

    Changed vercel.json based on a forum answer

--- api/index.js
+++
@@ -1,4 +0,0 @@
-const server = require('./server'); // Import your Express app
-const { createServer } = require('@vercel/node'); // Required for serverless
-
-module.exports = createServer(server);
